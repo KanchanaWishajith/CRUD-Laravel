33d00182f50851fee5981b3b8dedbe1132965e31
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-07-17 12:51:28 +0530

    Start to create a CRUD using laravel

---
+++ crud-app/app/Http/Controllers/Post2Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post2;
+use Illuminate\Http\Request;
+
+class Post2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post2 $post2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post2 $post2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post2 $post2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post2 $post2)
+    {
+        //
+    }
+}
